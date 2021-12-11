99012671db84466b587306f301d4b75476984b77
MatteoMaske <matteomascherin@gmail.com>
2021-12-11 15:45:39 +0100

    fix initialize fun

--- ui/script.js
+++ ui/script.js
@@ -1,3 +1,4 @@
+//INIZIALIZZO PAGINA CON LOGO E DIV PRINCIPALE
 const app = document.getElementById('root');
 
 const logo = document.createElement('img');
@@ -19,36 +20,12 @@ container.setAttribute('class', 'container');
 app.appendChild(logo);
 app.appendChild(container);
 
+//INIZIALIZZO LA PAGINA
 initialize();
 
-function changeContainer(ID){
-    container.removeChild(list);
-    const prova=document.createElement("ul");
-
-    var request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:49146/api/punti?id='+ID, true);
-    request.onload = function () {
-
-
-        // Begin accessing JSON data here
-        var data = JSON.parse(this.response);
-    //  console.log(data);
-        if (request.status >= 200 && request.status < 400) {
-            data[0].Punti.forEach(punto => {
-            console.log(punto);
-            let item=document.createElement('li');
-            item.textContent=(punto.NomePunto);
-
-            prova.appendChild(item);
-            });
-        }
-    }
-
-    request.send();
-    container.appendChild(prova);
-}
-
 function initialize(){
+
+    while(container.firstChild)container.removeChild(container.firstChild);
 
 const list = document.createElement('ul');
 list.setAttribute('class', 'list-group');
@@ -104,6 +81,36 @@ request.onload = function () {
         app.appendChild(errorMessage);
     }
 }
+
+//FUNZIONE CEH AL CLICK SUL PARCO VISUALIZZA PUNTI D'INTERESSE
+function changeContainer(ID){
+    while(container.firstChild)container.removeChild(container.firstChild);
+    const prova=document.createElement("ul");
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:49146/api/punti?id='+ID, true);
+    request.onload = function () {
+
+
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+    //  console.log(data);
+        if (request.status >= 200 && request.status < 400) {
+            data[0].Punti.forEach(punto => {
+            console.log(punto);
+            let item=document.createElement('li');
+            item.textContent=(punto.NomePunto);
+
+            prova.appendChild(item);
+            });
+        }
+    }
+
+    request.send();
+    container.appendChild(prova);
+}
+
+
 function disabledEventPropagation(event)
 {
    if (event.stopPropagation){
