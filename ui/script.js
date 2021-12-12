c3779b4ae785c6fe0caf62c3fb62ca770c6081c2
MatteoMaske <matteomascherin@gmail.com>
2021-12-12 17:19:23 +0100

    Aggiunta card

--- ui/script.js
+++ ui/script.js
@@ -5,9 +5,9 @@ const logo = document.createElement('img');
 logo.setAttribute('class','foto');
 logo.setAttribute('align','center');
 logo.src = '../logo.png';
-/*logo.onclick=()=>{
+logo.onclick=()=>{
     initialize();
-};*/
+};
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
@@ -22,41 +22,51 @@ function initialize(){
 
     while(container.firstChild)container.removeChild(container.firstChild);
 
-const list = document.createElement('ul');
-list.setAttribute('class', 'list-group');
-//container.removeChild(list);
+    const list = document.createElement('ul');
+    list.setAttribute('class', 'list-group');
+    //container.removeChild(list);
 
-container.appendChild(list);
+    container.appendChild(list);
 
-var request = new XMLHttpRequest();
-request.open('GET', 'http://localhost:49146/api/parchi', true);
-request.onload = function () {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:49146/api/parchi', true);
+    request.onload = function () {
 
 
         // Begin accessing JSON data here
         var data = JSON.parse(this.response);
-    //  console.log(data);
+        console.log(data);
         if (request.status >= 200 && request.status < 400) {
-            data[0].Punti.forEach(punto => {
-            console.log(punto);
+            data.forEach(parco => {
+            console.log(parco);
+
             let item=document.createElement('li');
-            item.textContent=(punto.NomePunto);
+            item.setAttribute('class','list-group-item d-flex justify-content-between align-items-center');
+            item.textContent=(parco.Nome);
+            item.onclick=()=>{
+               // e.stopPropagation;
+                changeContainer2(parco.Id);
+            }
 
-            prova2.appendChild(item);
-            item=document.createElement('li');
-            item.textContent=(punto.Coordinate.Lat);
+            let span=document.createElement('span');
+            span.setAttribute('class','badge bg-primary rounded-pill');
+            span.textContent=('☆');
+            span.onclick=()=>{
+               // e.stopPropagation;
+                console.log(e.target);
+                let star = span.textContent;
+                console.log(star);
+                changeStar(star,this);
+            }
             
-            prova2.appendChild(item);
-            item=document.createElement('li');
-            item.textContent=(punto.Coordinate.Long);
+            item.appendChild(span);
+            list.appendChild(item);
 
-            prova2.appendChild(item);
             });
         }
     }
 
     request.send();
-    container.appendChild(prova2);
 }
 
 //FUNZIONE CEH AL CLICK SUL PARCO VISUALIZZA PUNTI D'INTERESSE
@@ -85,6 +95,73 @@ function changeContainer(ID){
 
     request.send();
     container.appendChild(prova);
+}
+
+function changeContainer2(ID, NOMEP){
+    while(container.firstChild)container.removeChild(container.firstChild);
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:49146/api/punti?id='+ID, true);
+    request.onload = function () {
+
+
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+    //  console.log(data);
+        if (request.status >= 200 && request.status < 400) {
+            data[0].Punti.forEach(punto => {
+            console.log(punto);
+            
+            //card principale
+            const card=document.createElement('div');
+            card.setAttribute('class','card');
+            card.setAttribute('style','width: 18rem;');
+            //immagine
+            const image=document.createElement('img');
+            image.setAttribute('src','../logo.png');
+            image.setAttribute('class',"card-img-top");
+            card.appendChild(image);
+
+            //titolo e info parco
+            const cardBody=document.createElement('div');
+            cardBody.setAttribute('class','card-body');
+                        
+            //nome parco
+            let title=document.createElement('h5');
+            title.setAttribute('class','card-title')
+            title.textContent=(punto.NomePunto);
+            cardBody.appendChild(title);
+            
+            //sottotolo coordinate
+            title=document.createElement('h6');
+            title.setAttribute('class','card-title')
+            title.textContent=('Coordinate punto:');
+            cardBody.appendChild(title);
+
+           //latitudine
+            let item=document.createElement('p');
+            item.textContent=('Lat: '+punto.Coordinate.Lat + "\n"+punto.Coordinate.Long);
+            cardBody.appendChild(item);            
+        
+          //longitudine
+            item=document.createElement('p');
+            item.textContent=('Long: '+punto.Coordinate.Long);
+            cardBody.appendChild(item);
+
+            card.appendChild(cardBody);
+            container.appendChild(card);
+            });
+        }
+    }
+
+    request.send();
+}
+
+function changeStar(star,span){
+    if(star=='☆')span.textContent=('★');
+    else span.textContent=('☆');
+
+    disabledEventPropagation(span);
 }
 
 
